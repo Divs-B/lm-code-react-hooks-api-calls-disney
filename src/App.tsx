7813efc99aaee8d3dfba1a6c651d41c8ede13439
Divya <divyabhatt16@gmail.com>
2022-04-27 15:38:14 +0100

    api call using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Header from "./components/header";
+import CharacterContainer from "./components/character_container";
+import Navigation from "./components/navigation";
+import { DisneyCharacter } from "./disney_character";
+import axios from "axios";
 
-import './App.css';
-import React, { useState } from 'react';
-import Header from './components/header';
-import CharacterContainer from './components/character_container';
-import Navigation from './components/navigation';
-import { DisneyCharacter } from './disney_character';
+const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
-const App : React.FC = () => {
+  useEffect(() => {
+    getCharacters(1);
+  }, []);
 
-	const [currentPage, setCurrentPage] = useState<number>(1);
+  const getCharacters = async (pageNumber: number) => {
+    const apiResponse = await axios.get(
+      "http://api.disneyapi.dev/characters?page=" + pageNumber
+    );
+    setCharacters(apiResponse.data.data);
+    console.log("-------->>" + characters);
+  };
 
-  // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
+  return (
+    <div className="page">
+      <Header currentPage={currentPage} />
+      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <CharacterContainer characters={characters} />
+    </div>
+  );
+};
+
+export default App;
+
+/*
+{
       _id: 6,
       name: "'Olu Mel",
       imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
@@ -27,17 +49,4 @@ const App : React.FC = () => {
       name: "Ace",
       imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
     },
-  ]);
-
-  
-
-  return (
-    <div className="page">
-      <Header currentPage={currentPage} />
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer characters={characters} />
-    </div>
-  );
-}
-
-export default App;
+*/
